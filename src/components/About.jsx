671a6128e796a0274b472c9fa4921cb2ccf44de2
tayHolmes7 <taylorh2343@gmail.com>
2023-02-01 19:45:42 -0600

    adjust about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,10 +29,9 @@ const About = () => {
               of the team's leadership committee to becoming a middle school
               teacher and coach, leadership has always come naturally to me. I
               bring a strong work ethic, unparalleled patience, and the ability
-              to complete tasks in an unconventional environment. My attention
-              to detail, creativity and teamwork are my greatest strengths. I am
-              eager to apply my skills and become a part of your software
-              engineering team!
+              to complete tasks in an unconventional environment. With a keen
+              eye for detail, and my strengths in creativity and teamwork, I am
+              eager to apply my knowledge to become your next Software Engineer!
             </p>
           </div>
         </div>
