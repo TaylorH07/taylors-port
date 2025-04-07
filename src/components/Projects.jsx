82230d59fb9491870365c8f7a6101aa5d07cac0b
tayHolmes7 <taylorh2343@gmail.com>
2025-04-07 17:11:35 -0500

    added white background

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,97 +8,122 @@ import ddelivery from '../assets/doug-deliveriesSS.png'
 
 const Projects = () => {
   return (
-    <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#02020c]' >
-        <div className=' max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ' >
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#45ffd7] anaglyph ' >Projects</p>
-                <p className='py-6 '>  -- Check out some of my recent projects: </p>
+    <div
+      name='projects'
+      className='w-full md:h-screen text-gray-300 bg-[#02020c]'
+    >
+      <div className=' max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#45ffd7] anaglyph '>
+            Projects
+          </p>
+          <p className='py-6 '> -- Check out some of my recent projects: </p>
+        </div>
+        {/* Grid container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4  '>
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${MAF})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
+          >
+            {/* hover effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-gray-500 tracking-wider bg-white'>
+                Interactive Front-End
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href=' https://github.com/TaylorH07/movie-actor-finder'
+                  target='_blank'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
+                    {' '}
+                    Repo
+                  </button>
+                </a>
+              </div>
             </div>
-            {/* Grid container */}
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4  '>
+          </div>
 
-                {/* grid item */}
-                <div 
-                    style={{ backgroundImage: `url(${MAF})` }}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ' >
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-gray-500 tracking-wider ' >
-                            Interactive Front-End 
-                        </span>
-                        <div className='pt-8 text-center'>
-                            
-                            <a href=" https://github.com/TaylorH07/movie-actor-finder" target="_blank" >
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '> Repo</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${PPChron})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
+          >
+            {/* hover effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-gray-500 tracking-wider bg-white'>
+                Full-Stack Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://github.com/TaylorH07/pawprint-chronicles'
+                  target=' _blank'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
+                    {' '}
+                    Repo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
 
-                {/* grid item */}
-                <div 
-                    style={{ backgroundImage: `url(${PPChron})` }}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ' >
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-gray-500 tracking-wider ' >
-                            Full-Stack Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            
-                            <a href="https://github.com/TaylorH07/pawprint-chronicles" target=" _blank" >
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '> Repo</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${ddelivery})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
+          >
+            {/* hover effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-gray-500 tracking-wider bg-white'>
+                Full-Stack Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://github.com/TaylorH07/dougie-deliveries'
+                  target=' _blank'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
+                    {' '}
+                    Repo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
 
-                {/* grid item */}
-                <div 
-                    style={{ backgroundImage: `url(${ddelivery})` }}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ' >
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-gray-500 tracking-wider ' >
-                            Full-Stack Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            
-                            <a href="https://github.com/TaylorH07/dougie-deliveries" target=" _blank" >
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '> Repo</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                  
-                {/* grid item */}
-                <div 
-                    style={{ backgroundImage: `url(${WDash})` }}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ' >
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-gray-500 tracking-wider ' >
-                            Server-Side API
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="https://github.com/TaylorH07/weather-dashboard" target=" _blank" >
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '> Repo</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${WDash})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
+          >
+            {/* hover effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-gray-500 tracking-wider bg-white'>
+                Server-Side API
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://github.com/TaylorH07/weather-dashboard'
+                  target=' _blank'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
+                    {' '}
+                    Repo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
 
-                {/* grid item */}
-                {/* <div 
+          {/* grid item */}
+          {/* <div 
                     style={{ backgroundImage: `url(${TeamGen})` }}
                     className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ' >
                     
                     {/* hover effects */}
-                    {/* <div className='opacity-0 group-hover:opacity-100 '>
+          {/* <div className='opacity-0 group-hover:opacity-100 '>
                         <span className='text-2xl font-bold text-gray-500 tracking-wider ' >
                             Object-Oriented Program
                         </span>
@@ -113,47 +138,57 @@ const Projects = () => {
                     </div>
                 </div> */}
 
-                {/* grid item */}
-                <div 
-                    style={{ backgroundImage: `url(${workdaySS})` }}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ' >
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-gray-500 tracking-wider ' >
-                            Third-Party API
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="https://github.com/TaylorH07/workday-scheduler" target=" _blank" >
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '> Repo</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                {/* grid item */}
-                <div 
-                    style={{ backgroundImage: `url(${NoteSS})` }}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div ' >
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100 '>
-                        <span className='text-2xl font-bold text-gray-500 tracking-wider'>
-                            Express.js Application
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="https://github.com/TaylorH07/note-taker" target=" _blank" >
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '> Repo</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${workdaySS})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
+          >
+            {/* hover effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-gray-500 tracking-wider bg-white'>
+                Third-Party API
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://github.com/TaylorH07/workday-scheduler'
+                  target=' _blank'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
+                    {' '}
+                    Repo
+                  </button>
+                </a>
+              </div>
             </div>
+          </div>
 
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${NoteSS})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
+          >
+            {/* hover effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-gray-500 tracking-wider bg-white'>
+                Express.js Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://github.com/TaylorH07/note-taker'
+                  target=' _blank'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>
+                    {' '}
+                    Repo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
 export default Projects
