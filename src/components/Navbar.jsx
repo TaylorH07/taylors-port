0d66d03ceec27660c3e958f28691cef4ba5409e2
tayHolmes7 <taylorh2343@gmail.com>
2025-04-07 17:38:30 -0500

    added resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,102 +11,123 @@ const Navbar = () => {
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#02020c] text-gray-300 z-10'>
-        <div>
-            <img src={Logo} alt="Logo" style={{width: '50px'}} />
-        </div>
+      <div>
+        <img src={Logo} alt='Logo' style={{ width: '50px' }} />
+      </div>
 
-        {/* Menu */}
-        <ul className='hidden md:flex'>
-            <li className='anaglyph'>
-                <Link to="home" smooth={true} duration={500}>
-                    Home
-                </Link>
-            </li>
-            <li className='anaglyph'>
-                <Link to="about" smooth={true} duration={500}>
-                    About
-                </Link>
-            </li>
-            <li className='anaglyph'>
-                <Link to="skills" smooth={true} duration={500}>
-                    Skills
-                </Link>
-            </li>
-            <li className='anaglyph'>
-                <Link to="projects" smooth={true} duration={500}>
-                    Projects
-                </Link>
-            </li>
-            <li className='anaglyph'>
-                <Link to="contact" smooth={true} duration={500}>
-                    Contact
-                </Link>
-            </li> 
-        </ul>
+      {/* Menu */}
+      <ul className='hidden md:flex'>
+        <li className='anaglyph'>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='anaglyph'>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='anaglyph'>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='anaglyph'>
+          <Link to='projects' smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li className='anaglyph'>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
 
-        {/* Hamburger */}
-        <div onClick={handleClick} className='md:hidden z-10'>
-            {!nav ? <FaBars /> : <FaTimes />}
-        </div>
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
 
-        {/* Mobile Menu */}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className = 'py-6 text-4xl '>
-                <Link onClick={handleClick} to="home" smooth={true} duration={500}>
-                    Home
-                </Link>
-            </li>
-            <li className = 'py-6 text-4xl'>
-                <Link onClick={handleClick} to="about" smooth={true} duration={500}>
-                    About
-                </Link>
-            </li>
-            <li className = 'py-6 text-4xl'>
-                <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
-                    Skills
-                </Link>
-            </li>
-            <li className = 'py-6 text-4xl'>
-                <Link onClick={handleClick} to="projects" smooth={true} duration={500}>
-                    Projects
-                </Link>
-            </li>
-            <li className = 'py-6 text-4xl'>
-                <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
-                    Contact
-                </Link>
-            </li> 
-        </ul>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl '>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link
+            onClick={handleClick}
+            to='projects'
+            smooth={true}
+            duration={500}
+          >
+            Projects
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
 
-        {/* Social Icons */}
-        <div className ='hidden lg:flex fixed flex-col top-[35%] left-0' >
-            <ul>
-                <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500 '>
-                    <a className='flex justify-between items-center w-full text-gray-300 ' href=" https://linkedin.com/in/taylor-holmes-75a661186" target="_blank">
-                        LinkedIn <FaLinkedin size={30} />
-                    </a>
-                </li>
-                <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#3333] '>
-                    <a className='flex justify-between items-center w-full text-gray-300 ' href="https://github.com/TaylorH07" rel="noreferrer" target="_blank">
-                        Github <FaGithub size={30} />
-                    </a>
-                </li>
-                {/* <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#66ad9c] '>
+      {/* Social Icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500 '>
+            <a
+              className='flex justify-between items-center w-full text-gray-300 '
+              href=' https://linkedin.com/in/taylor-holmes-75a661186'
+              target='_blank'
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#3333] '>
+            <a
+              className='flex justify-between items-center w-full text-gray-300 '
+              href='https://github.com/TaylorH07'
+              rel='noreferrer'
+              target='_blank'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          {/* <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#66ad9c] '>
                     <a className='flex justify-between items-center w-full text-gray-300 ' href="/">
                         Email <HiOutlineMail size={30} />
                     </a>
                 </li> */}
-                <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#9381ff] '>
-                    <a className='flex justify-between items-center w-full text-gray-300 ' href="https://docs.google.com/document/d/1UVBFZn6hx4ORGUutvimE8Q4dAClsF8eDect_5zEZCl8/edit?usp=sharing">
-                        Resume <BsFillPersonLinesFill size={30} />
-                    </a>
-                </li>
-            </ul>
-        </div>
-
-
+          <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#9381ff] '>
+            <a
+              className='flex justify-between items-center w-full text-gray-300 '
+              href='https://drive.google.com/file/d/1LtzDasme0lz9vf8qDcS5VfIibtTY0L5c/view?usp=sharing'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
