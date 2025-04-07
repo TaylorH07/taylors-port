e60b224c611ce49b085dcea312643e5ba918669a
tayHolmes7 <taylorh2343@gmail.com>
2025-04-07 17:30:51 -0500

    adjusted title and about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,36 +2,33 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-[#02020c] text-gray-300">
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 ">
-          <div className="sm:text-right pb-8 pl-4 ">
-            <p className="text-4xl font-bold inline border-b-4 border-[#45ffd7] anaglyph">
+    <div name='about' className='w-full h-screen bg-[#02020c] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8 '>
+          <div className='sm:text-right pb-8 pl-4 '>
+            <p className='text-4xl font-bold inline border-b-4 border-[#45ffd7] anaglyph'>
               About
             </p>
           </div>
           <div></div>
         </div>
-        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 ">
-          <div className=" sm:text-right text-4xl font-bold ">
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 '>
+          <div className=' sm:text-right text-4xl font-bold '>
             <p>
-              {" "}
-              Hey, I am ecstatic that you are here! Please take a look around.{" "}
+              {' '}
+              Hey, I am ecstatic that you are here! Please take a look around.{' '}
             </p>
           </div>
           <div>
             <p>
-              -- I graduated from SMU’s Full Stack Web Development Program while
-              working as a full time special education inclusion teacher and
-              coaching multiple sports. As someone who comes from a strong
-              educational background, hardwork and the drive to learn never
-              ends. From being a collegiate football team captain and a member
-              of the team's leadership committee to becoming a middle school
-              teacher and coach, leadership has always come naturally to me. I
-              bring a strong work ethic, unparalleled patience, and the ability
-              to complete tasks in an unconventional environment. With a keen
-              eye for detail, and my strengths in creativity and teamwork, I am
-              eager to apply my knowledge to become your next Software Engineer!
+              -- Results-driven Learning & Development Specialist with
+              experience in training, instructional design, and content
+              development. Skilled at delivering engaging training programs in
+              both virtual and in-person settings and leveraging adult learning
+              principles to enhance knowledge retention. Skilled in curriculum
+              development, Learning Management System (LMS) administration, and
+              training evaluation – ensuring effective learning experiences that
+              align with organizational goals. 
             </p>
           </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,38 +5,38 @@ import jellyFish from "../assets/jellyfishVid.mp4";
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen relative">
+    <div name='home' className='w-full h-screen relative'>
       <video
-        className="w-full h-full object-cover "
+        className='w-full h-full object-cover '
         src={jellyFish}
         autoPlay
         loop
         muted
       />
 
-      <div className="absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
+      <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
       {/* Container */}
-      <div className="absolute top-0 max-w-[1000px] px-10 flex flex-col justify-center h-full mx-14 ">
-        <p className="text-[#c0fdff]"> Hey hey everyone, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#b8c6f4] ">
-          {" "}
-          Taylor Holmes.{" "}
+      <div className='absolute top-0 max-w-[1000px] px-10 flex flex-col justify-center h-full mx-14 '>
+        <p className='text-[#c0fdff]'> Hey hey everyone, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#b8c6f4] '>
+          {' '}
+          Taylor Holmes.{' '}
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#869ce4] ">
-          {" "}
-          I am your next Developer/ Engineer; Here's why...
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#869ce4] '>
+          {' '}
+          I am your next Learning Specialist; Here's why...
         </h2>
-        <p className="text-[#869ce4] py-4 max-w-[700px] ">
-          -- I'm passionate about problem solving and engineering resilient
-          technology to improve the lives of users around the world.
+        <p className='text-[#869ce4] py-4 max-w-[700px] '>
+          -- Passionate about facilitating impactful learning initiatives that
+          drive performance and professional growth.
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#869ce4] hover:border-[#b8c6f4] anaglyph">
-            <Link to="projects" smooth={true} duration={500}>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#869ce4] hover:border-[#b8c6f4] anaglyph'>
+            <Link to='projects' smooth={true} duration={500}>
               View Projects
             </Link>
-            <span className="group-hover:rotate-90 duration-300 ">
-              <HiArrowNarrowRight className="ml-3 " />
+            <span className='group-hover:rotate-90 duration-300 '>
+              <HiArrowNarrowRight className='ml-3 ' />
             </span>
           </button>
         </div>
